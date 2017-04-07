84851c61d84a096dc66e26e6424d7dae7518a445
dfghjm <popov@ucu.edu.ua>
2017-04-07 23:36:34 +0300

    coursework

--- modules/pyslideshare/xml2dict.py
+++ modules/pyslideshare/xml2dict.py
@@ -4,14 +4,14 @@
 #
 
 import xml.etree.ElementTree as ET
-from object_dict import object_dict 
+from .object_dict import object_dict 
 
 def __parse_node(node):
     tmp = object_dict()
     # save attrs and text, hope there will not be a child with same name
     if node.text:
         tmp['value'] = node.text
-    for (k,v) in node.attrib.items():
+    for (k,v) in list(node.attrib.items()):
         tmp[k] = v
 
     for ch in node.getchildren():
@@ -54,8 +54,8 @@ if __name__ == '__main__':
     import pprint
     pprint.pprint(r)
 
-    print r.result.count.value
-    print r.result.count.n
+    print(r.result.count.value)
+    print(r.result.count.n)
 
     for data in r.result.data:
-        print data.id, data.name 
+        print(data.id, data.name) 
